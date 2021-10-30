fd7f99644cd12c6d96072efcdac28485b2db2cb1
iman-aazibou <imanaazibou@hotmail.com>
2021-10-30 23:26:06 +0200

    added userInfo component and function to set user data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import AddUser from './Components/Users/AddUser';
-
+import AddUser from "./Components/Users/AddUser";
+import UserInfo from "./Components/Users/UserInfo";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: userName, age: userAge, id: Math.random() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser/>
+      <AddUser onAddUser={addUserHandler} />
+      <UserInfo users={usersList} />
     </div>
   );
 }
